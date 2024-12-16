076720689c0693de3d46296dd56c2eaac7da8665
grisottoc <sales.mercadodirecto@gmail.com>
2024-12-16 15:53:54 +0000

    updateGrisotto

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function getRandomElement(arr) {
+    const index = Math.floor(Math.random() * arr.length);
+    return arr[index];
+  }
+
+  function generateRandomExcuse() {
+    let who = ["The dog", "My grandma", "The mailman", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "my phone", "the car"];
+    let when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    let excuse = `${getRandomElement(who)} ${getRandomElement(
+      action
+    )} ${getRandomElement(what)} ${getRandomElement(when)}`;
+
+    document.getElementById("excuse").innerHTML = excuse;
+  }
+
+  generateRandomExcuse();
 };
